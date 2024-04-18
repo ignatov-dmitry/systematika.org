c323da0a4923be4efff7847487d68d0a6ff2a950
Dmitry <ignatov.d43@gmail.com>
2024-04-18 10:25:33 +0300

    Добавление фильтра по дате логов геткурса [2]

--- integration/app/Http/Controllers/GKLogController.php
+++ integration/app/Http/Controllers/GKLogController.php
@@ -3,13 +3,32 @@
 namespace App\Http\Controllers;
 
 use App\Models\GKUpdateLog;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class GKLogController extends Controller
 {
-    public function list()
+    public function list(Request $request)
     {
-        $logs = GKUpdateLog::with('user')->orderBy('date_create', 'DESC')->paginate(25);
+
+        $minDateTimestamp = Carbon::parse($request->get('date'))
+            ->hour(0)
+            ->setMinute(0)
+            ->setSecond(0)
+            ->timestamp;
+
+        $maxDateTimestamp = Carbon::parse($request->get('date'))
+            ->hour(23)
+            ->setMinute(59)
+            ->setSecond(59)
+            ->timestamp;
+
+        $logs = GKUpdateLog::with('user')
+            ->orderBy('date_create', 'DESC')
+            ->where('date_create', '>=', $minDateTimestamp)
+            ->where('date_create', '<=', $maxDateTimestamp)
+            ->paginate(25)
+            ->withQueryString();
 
         return view('logs.getcourse.list', compact('logs'));
     }
--- integration/resources/views/logs/getcourse/list.blade.php
+++ integration/resources/views/logs/getcourse/list.blade.php
@@ -1,6 +1,31 @@
+@php
+use Illuminate\Support\Carbon;
+
+$prevDay = Carbon::parse(request('date'))->subDay()->format('Y-m-d');
+$currDay = Carbon::parse(request('date'))->format('Y-m-d');
+$nextDay = Carbon::parse(request('date'))->addDay()->format('Y-m-d');
+
+@endphp
 @extends('layouts.app')
 @section('content')
     <div class="container mx-auto px-4">
+        <nav class="flex justify-center m-5" aria-label="Page navigation example">
+            <ul class="inline-flex -space-x-px text-base h-10">
+                <li>
+                    <a href="?date={{ $prevDay }}" class="flex items-center justify-center px-4 h-10 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Previous</a>
+                </li>
+                <li>
+                    <div class="grid gap-6 mb-6 md:grid-cols-2">
+                        <div>
+                            <input value="{{ request('date') ?? $currDay }}" type="date" name="date" class="bg-gray-50 border-gray-300 text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="John" required="">
+                        </div>
+                    </div>
+                </li>
+                <li>
+                    <a href="?date={{ $nextDay }}" class="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Next</a>
+                </li>
+            </ul>
+        </nav>
         <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
             <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                 <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
